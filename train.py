a2d379b078d6a76ed906bc3b35ed5bf62f8dabeb
Nexround <527159991@qq.com>
2023-03-15 22:34:49 +0800

    fix

--- train.py
+++ train.py
@@ -183,7 +183,7 @@ for epoch in range(opt.epoch, opt.n_epochs):
             ims_dict[label] = wandb_image
         img_list = ims_dict.values()
         result_table.add_data(epoch, *img_list)
-        wandb.log(wandb.Image(fake_B))
+        wandb.log(wandb.Image({"fake_B": fake_B}))
         wandb.log({'loss_G': loss_G, 'loss_G_identity': (loss_identity_A + loss_identity_B), 'loss_G_GAN': (loss_GAN_A2B + loss_GAN_B2A),
                     'loss_G_cycle': (loss_cycle_ABA + loss_cycle_BAB), 'loss_D': (loss_D_A + loss_D_B)}, 
                     )
