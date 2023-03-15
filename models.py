4a9e49112cf9e38fc3c3a3c6f0075d684826405a
Nexround <527159991@qq.com>
2023-03-15 18:29:37 +0800

    stop using animeGAN discriminator

--- models.py
+++ models.py
@@ -82,72 +82,72 @@ def initialize_weights(net):
             # print(f'SKip layer {m}, {e}')
             pass
 
-class Discriminator(nn.Module):
-    def __init__(self,  args):
-        super(Discriminator, self).__init__()
-        # self.name = f'discriminator_{args.dataset}'
-        self.bias = False
-        channels = 32
-
-        layers = [
-            nn.Conv2d(3, channels, kernel_size=3, stride=1, padding=1, bias=self.bias),
-            nn.LeakyReLU(0.2, True)
-        ]
-
-        for i in range(3):
-            layers += [
-                nn.Conv2d(channels, channels * 2, kernel_size=3, stride=2, padding=1, bias=self.bias),
-                nn.LeakyReLU(0.2, True),
-                nn.Conv2d(channels * 2, channels * 4, kernel_size=3, stride=1, padding=1, bias=self.bias),
-                nn.InstanceNorm2d(channels * 4),
-                nn.LeakyReLU(0.2, True),
-            ]
-            channels *= 4
-
-        layers += [
-            nn.Conv2d(channels, channels, kernel_size=3, stride=1, padding=1, bias=self.bias),
-            nn.InstanceNorm2d(channels),
-            nn.LeakyReLU(0.2, True),
-            nn.Conv2d(channels, 1, kernel_size=3, stride=1, padding=1, bias=self.bias),
-        ]
-
-        if True:
-            for i in range(len(layers)):
-                if isinstance(layers[i], nn.Conv2d):
-                    layers[i] = spectral_norm(layers[i])
-
-        self.discriminate = nn.Sequential(*layers)
-
-        initialize_weights(self)
-
-    def forward(self, img):
-        return self.discriminate(img)
 # class Discriminator(nn.Module):
-#     def __init__(self, input_nc):
+#     def __init__(self,  args):
 #         super(Discriminator, self).__init__()
+#         # self.name = f'discriminator_{args.dataset}'
+#         self.bias = False
+#         channels = 32
 
-#         # A bunch of convolutions one after another
-#         model = [   nn.Conv2d(input_nc, 64, 4, stride=2, padding=1),
-#                     nn.LeakyReLU(0.2, inplace=True) ]
+#         layers = [
+#             nn.Conv2d(3, channels, kernel_size=3, stride=1, padding=1, bias=self.bias),
+#             nn.LeakyReLU(0.2, True)
+#         ]
 
-#         model += [  nn.Conv2d(64, 128, 4, stride=2, padding=1),
-#                     nn.InstanceNorm2d(128), 
-#                     nn.LeakyReLU(0.2, inplace=True) ]
+#         for i in range(3):
+#             layers += [
+#                 nn.Conv2d(channels, channels * 2, kernel_size=3, stride=2, padding=1, bias=self.bias),
+#                 nn.LeakyReLU(0.2, True),
+#                 nn.Conv2d(channels * 2, channels * 4, kernel_size=3, stride=1, padding=1, bias=self.bias),
+#                 nn.InstanceNorm2d(channels * 4),
+#                 nn.LeakyReLU(0.2, True),
+#             ]
+#             channels *= 4
 
-#         model += [  nn.Conv2d(128, 256, 4, stride=2, padding=1),
-#                     nn.InstanceNorm2d(256), 
-#                     nn.LeakyReLU(0.2, inplace=True) ]
+#         layers += [
+#             nn.Conv2d(channels, channels, kernel_size=3, stride=1, padding=1, bias=self.bias),
+#             nn.InstanceNorm2d(channels),
+#             nn.LeakyReLU(0.2, True),
+#             nn.Conv2d(channels, 1, kernel_size=3, stride=1, padding=1, bias=self.bias),
+#         ]
 
-#         model += [  nn.Conv2d(256, 512, 4, padding=1),
-#                     nn.InstanceNorm2d(512), 
-#                     nn.LeakyReLU(0.2, inplace=True) ]
+#         if True:
+#             for i in range(len(layers)):
+#                 if isinstance(layers[i], nn.Conv2d):
+#                     layers[i] = spectral_norm(layers[i])
 
-#         # FCN classification layer
-#         model += [nn.Conv2d(512, 1, 4, padding=1)]
+#         self.discriminate = nn.Sequential(*layers)
 
-#         self.model = nn.Sequential(*model)
+#         initialize_weights(self)
 
-#     def forward(self, x):
-#         x =  self.model(x)
-#         # Average pooling and flatten
-#         return F.avg_pool2d(x, x.size()[2:]).view(x.size()[0], -1)
+#     def forward(self, img):
+#         return self.discriminate(img)
+class Discriminator(nn.Module):
+    def __init__(self, input_nc):
+        super(Discriminator, self).__init__()
+
+        # A bunch of convolutions one after another
+        model = [   nn.Conv2d(input_nc, 64, 4, stride=2, padding=1),
+                    nn.LeakyReLU(0.2, inplace=True) ]
+
+        model += [  nn.Conv2d(64, 128, 4, stride=2, padding=1),
+                    nn.InstanceNorm2d(128), 
+                    nn.LeakyReLU(0.2, inplace=True) ]
+
+        model += [  nn.Conv2d(128, 256, 4, stride=2, padding=1),
+                    nn.InstanceNorm2d(256), 
+                    nn.LeakyReLU(0.2, inplace=True) ]
+
+        model += [  nn.Conv2d(256, 512, 4, padding=1),
+                    nn.InstanceNorm2d(512), 
+                    nn.LeakyReLU(0.2, inplace=True) ]
+
+        # FCN classification layer
+        model += [nn.Conv2d(512, 1, 4, padding=1)]
+
+        self.model = nn.Sequential(*model)
+
+    def forward(self, x):
+        x =  self.model(x)
+        # Average pooling and flatten
+        return F.avg_pool2d(x, x.size()[2:]).view(x.size()[0], -1)
