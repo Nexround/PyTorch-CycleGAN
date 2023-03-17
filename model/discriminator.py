162a1c2fafb98c4a7dd84c56087be1a92d979239
Nexround <527159991@qq.com>
2023-03-17 22:48:55 +0800

    use cartoongan discriminator

--- model/discriminator.py
+++ model/discriminator.py
@@ -1,8 +1,60 @@
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.nn.utils import spectral_norm
-
 class Discriminator(nn.Module):
+    # initializers
+    def __init__(self, nf=32):
+        super(Discriminator, self).__init__()
+
+        self.nf = nf
+        self.convs = nn.Sequential(
+            nn.Conv2d(3, nf, 3, 1, 1),
+            nn.LeakyReLU(0.2, True),
+            nn.Conv2d(nf, nf * 2, 3, 2, 1),
+            nn.LeakyReLU(0.2, True),
+            nn.Conv2d(nf * 2, nf * 4, 3, 1, 1),
+            nn.InstanceNorm2d(nf * 4),
+            nn.LeakyReLU(0.2, True),
+            nn.Conv2d(nf * 4, nf * 4, 3, 2, 1),
+            nn.LeakyReLU(0.2, True),
+            nn.Conv2d(nf * 4, nf * 8, 3, 1, 1),
+            nn.InstanceNorm2d(nf * 8),
+            nn.LeakyReLU(0.2, True),
+            nn.Conv2d(nf * 8, nf * 8, 3, 1, 1),
+            nn.InstanceNorm2d(nf * 8),
+            nn.LeakyReLU(0.2, True),
+            nn.Conv2d(nf * 8, 1, 3, 1, 1),
+            nn.Sigmoid(),
+        )
+
+        self.initialize_weights(self)
+
+    def initialize_weights(self, net):   
+        for m in net.modules():
+            try:
+                if isinstance(m, nn.Conv2d):
+                    m.weight.data.normal_(0, 0.02)
+                    m.bias.data.zero_()
+                elif isinstance(m, nn.ConvTranspose2d):
+                    m.weight.data.normal_(0, 0.02)
+                    m.bias.data.zero_()
+                elif isinstance(m, nn.Linear):
+                    m.weight.data.normal_(0, 0.02)
+                    m.bias.data.zero_()
+                elif isinstance(m, nn.BatchNorm2d):
+                    m.weight.data.fill_(1)
+                    m.bias.data.zero_()
+            except Exception as e:
+                # print(f'SKip layer {m}, {e}')
+                pass
+    # forward method
+    def forward(self, input):
+        # input = torch.cat((input1, input2), 1)
+        output = self.convs(input)
+
+        return output
+    
+class _Discriminator(nn.Module):
     def __init__(self, opt):
         super(Discriminator, self).__init__()
         self.bias = False
